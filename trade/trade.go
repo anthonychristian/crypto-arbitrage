307ddf811726ab191c944b2a6a7d6be20ec35ad9
Putri Karunia <putri@berkeley.edu>
2019-01-07 11:05:39 +0700

    save calculations inside array, decrease orderbook qty

--- trade/trade.go
+++ trade/trade.go
@@ -2,6 +2,7 @@ package trade
 
 import (
 	"errors"
+	"fmt"
 	"math"
 	"strconv"
 
@@ -20,6 +21,45 @@ type OwnedCurr struct {
 	funds float64 // to calculate how much to buy
 }
 
+type CalcParts struct {
+	operator  string
+	amt       float64
+	nextCurr  orderbook.Currency
+	prevCurr  orderbook.Currency
+	action    string
+	orderbook *orderbook.OrderBook
+}
+
+func execCalc(a float64, b CalcParts) (float64, error) {
+	switch b.operator {
+	case "+":
+		return a + b.amt, nil
+	case "-":
+		return a - b.amt, nil
+	case "*":
+		return a * b.amt, nil
+	case "/":
+		return a / b.amt, nil
+	default:
+		return 0, errors.New("operator invalid")
+	}
+}
+
+func execReverseCalc(a float64, b CalcParts) (float64, error) {
+	switch b.operator {
+	case "+":
+		return a - b.amt, nil
+	case "-":
+		return a + b.amt, nil
+	case "*":
+		return a / b.amt, nil
+	case "/":
+		return a * b.amt, nil
+	default:
+		return 0, errors.New("operator invalid")
+	}
+}
+
 func detectArbitrage(pairs []Pair) (start float64, end float64, qty float64, err error) {
 	// starting currency set owned = 1
 	// if sell = left curr
@@ -34,6 +74,7 @@ func detectArbitrage(pairs []Pair) (start float64, end float64, qty float64, err
 		owned.curr = orderbook.GetRightCurrency(pairs[0].symbol)
 		startCurr = orderbook.GetRightCurrency(pairs[0].symbol)
 	}
+	calcs := []CalcParts{}
 
 	for index, pair := range pairs {
 		ob := orderbook.Exchanges[pair.exchange].Books[pair.symbol]
@@ -50,6 +91,14 @@ func detectArbitrage(pairs []Pair) (start float64, end float64, qty float64, err
 			owned.qty = owned.qty / bestPrice.Price
 			// max qty you can buy = max(available quantity at the price, funds you have)
 			owned.funds = math.Min(bestPrice.Qty, owned.funds/bestPrice.Price)
+			calcs = append(calcs, CalcParts{
+				operator:  "/",
+				amt:       bestPrice.Price,
+				prevCurr:  rightCurr,
+				nextCurr:  leftCurr,
+				action:    "buy",
+				orderbook: ob,
+			})
 		} else {
 			if owned.curr != leftCurr {
 				return 0, 0, 0, errors.New("wrong starting currency " + string(owned.curr))
@@ -60,11 +109,28 @@ func detectArbitrage(pairs []Pair) (start float64, end float64, qty float64, err
 			owned.qty = owned.qty * bestPrice.Price
 			// max qty you can sell = max(available quantity at the price, funds you have)
 			owned.funds = math.Min(bestPrice.Qty, owned.funds) * bestPrice.Price
+			calcs = append(calcs, CalcParts{
+				operator:  "*",
+				amt:       bestPrice.Price,
+				prevCurr:  leftCurr,
+				nextCurr:  rightCurr,
+				action:    "sell",
+				orderbook: ob,
+			})
 		}
 
 		// add taker price
-		owned.qty = owned.qty * (1 - orderbook.ExFeeMap[pair.exchange])
-		owned.funds = owned.funds * (1 - orderbook.ExFeeMap[pair.exchange])
+		exFee := (1 - orderbook.ExFeeMap[pair.exchange])
+		owned.qty = owned.qty * exFee
+		owned.funds = owned.funds * exFee
+		calcs = append(calcs, CalcParts{
+			operator:  "*",
+			amt:       exFee,
+			prevCurr:  owned.curr,
+			nextCurr:  owned.curr,
+			action:    "exchangeFee",
+			orderbook: ob,
+		})
 
 		// add withdraw fee if transfering to next exchange
 		nextPair := pairs[0]
@@ -83,10 +149,18 @@ func detectArbitrage(pairs []Pair) (start float64, end float64, qty float64, err
 				return 0, 0, 0, err
 			}
 			owned.funds -= withdrawFee
+			calcs = append(calcs, CalcParts{
+				operator:  "-",
+				amt:       withdrawFee,
+				prevCurr:  owned.curr,
+				nextCurr:  owned.curr,
+				action:    "withdrawFee",
+				orderbook: ob,
+			})
 		}
 	}
 	if owned.curr != startCurr {
-		return 0, owned.qty, 0, errors.New("Cycle incomplete! Started with " + string(startCurr) + " end with " + string(owned.curr))
+		return 0, 0, 0, errors.New("Cycle incomplete! Started with " + string(startCurr) + " end with " + string(owned.curr))
 	}
 
 	endingFund := owned.funds
@@ -94,48 +168,31 @@ func detectArbitrage(pairs []Pair) (start float64, end float64, qty float64, err
 	// if profitable
 	if owned.qty > 1000 && owned.funds > 0 {
 		// trace back needed funds from ending fund
-		for i := len(pairs) - 1; i >= 0; i-- {
-			pair := pairs[i]
-			ob := orderbook.Exchanges[pair.exchange].Books[pair.symbol]
-			leftCurr := orderbook.GetLeftCurrency(pair.symbol)
-			rightCurr := orderbook.GetRightCurrency(pair.symbol)
-			var bestPrice orderbook.Order
-			nextPair := pairs[0]
-			if i < len(pairs)-1 {
-				nextPair = pairs[i+1]
+		for i := len(calcs) - 1; i >= 0; i-- {
+			b := calcs[i]
+			endFund := owned.funds
+			owned.funds, err = execReverseCalc(owned.funds, b)
+			if err != nil {
+				return 0, 0, 0, err
 			}
-			if nextPair.exchange != pair.exchange {
-				// for funds calculation
-				withdrawFee, err := orderbook.WithdrawFee(pair.exchange, owned.curr, owned.funds)
-				if err != nil {
-					return 0, 0, 0, err
-				}
-				// get original fund before withdraw fee
-				owned.funds += withdrawFee
-			}
-			// get original fund before taker fee
-			owned.funds = owned.funds / (1 - orderbook.ExFeeMap[pair.exchange])
+			owned.curr = b.prevCurr
 
-			if pair.side == Buy {
-				if owned.curr != leftCurr {
-					return 0, 0, 0, errors.New("traceback: wrong starting currency " + string(owned.curr))
-				}
-				bestPrice = ob.LowPriceSellSide()
-				// if buy -> originally had right Curr
-				owned.curr = rightCurr
-				// original fund of right Curr = left Curr * price
-				owned.funds = owned.funds * bestPrice.Price
-			} else {
-				if owned.curr != rightCurr {
-					return 0, 0, 0, errors.New("traceback: wrong starting currency " + string(owned.curr))
-				}
-				bestPrice = ob.TopPriceBuySide()
-				// if sell -> orifinally had left curr
-				owned.curr = leftCurr
-				// original funds of left Curr = right Curr / price
-				owned.funds = owned.funds / bestPrice.Price
+			// decrease qty in orderbook
+			if b.action == "buy" {
+				bestPrice := b.orderbook.LowPriceSellSide()
+				// buying qty = amount received = endFund
+				bestPrice.Qty -= endFund
+				b.orderbook.AddSell(bestPrice)
+				bestPrice = b.orderbook.LowPriceSellSide()
+				fmt.Println(bestPrice)
+			} else if b.action == "sell" {
+				bestPrice := b.orderbook.TopPriceBuySide()
+				// selling qty = amount sold (amount had originally) = owned.funds
+				bestPrice.Qty -= owned.funds
+				b.orderbook.AddBuy(bestPrice)
+				bestPrice = b.orderbook.TopPriceBuySide()
+				fmt.Println(bestPrice)
 			}
-
 		}
 		if owned.curr != startCurr {
 			return owned.funds, endingFund, owned.qty, errors.New("Traceback cycle wrong! Started with " + string(startCurr) + " end with " + string(owned.curr))
--- trade/trade_test.go
+++ trade/trade_test.go
@@ -26,7 +26,7 @@ func (s *TradeSuite) SetupSuite() {
 func (s *TradeSuite) TearDownSuite() {}
 
 func (s *TradeSuite) TestTrade() {
-	CreateDummyExchangesNotProfitable()
+	best1, best2, best3 := CreateDummyExchangesNotProfitable()
 	pair1 := Pair{
 		symbol:   orderbook.ETH_USDT,
 		exchange: orderbook.Binance,
@@ -50,8 +50,11 @@ func (s *TradeSuite) TestTrade() {
 	assert.True(err != nil)
 	assert.Equal(990.5135725590688, qty)
 
-	CreateDummyExchangesNotProfitableQty()
+	best1, best2, best3 = CreateDummyExchangesNotProfitableQty()
 	start, end, qty, err = detectArbitrage([]Pair{pair1, pair2, pair3})
+	binance_eth_usdt := orderbook.Exchanges[orderbook.Binance].Books[orderbook.ETH_USDT]
+	idx_eth_idr := orderbook.Exchanges[orderbook.Indodax].Books[orderbook.ETH_IDR]
+	idx_usdt_idr := orderbook.Exchanges[orderbook.Indodax].Books[orderbook.USDT_IDR]
 	s.T().Log(start)
 	s.T().Log(end)
 	s.T().Log(qty)
@@ -59,17 +62,59 @@ func (s *TradeSuite) TestTrade() {
 	assert.Equal(1021.5982950714285, qty)
 	assert.Equal(((((start/150)*(1-0.001))-0.01)*2283000*(1-0.003)/14700*(1-0.003))-5, end)
 
-	CreateDummyExchangesProfitable()
+	receivedETH := start / 150
+	if best1.Qty-receivedETH == 0 {
+		assert.NotEqual(best1.Price, binance_eth_usdt.LowPriceSellSide().Price)
+	} else {
+		assert.Equal(best1.Qty-receivedETH, binance_eth_usdt.LowPriceSellSide().Qty)
+	}
+	soldETH := receivedETH*(1-0.001) - 0.01
+	if best2.Qty-soldETH == 0 {
+		assert.NotEqual(best2.Price, idx_eth_idr.TopPriceBuySide().Price)
+	} else {
+		assert.Equal(best2.Qty-soldETH, idx_eth_idr.TopPriceBuySide().Qty)
+	}
+	receivedUSDT := soldETH * 2283000 * (1 - 0.003) / 14700 //before fees
+	if best3.Qty-receivedUSDT == 0 {
+		assert.NotEqual(best3.Price, idx_usdt_idr.LowPriceSellSide().Price)
+	} else {
+		assert.Equal(best3.Qty-receivedUSDT, idx_usdt_idr.LowPriceSellSide().Qty)
+	}
+
+	best1, best2, best3 = CreateDummyExchangesProfitable()
 	start, end, qty, err = detectArbitrage([]Pair{pair1, pair2, pair3})
+	binance_eth_usdt = orderbook.Exchanges[orderbook.Binance].Books[orderbook.ETH_USDT]
+	idx_eth_idr = orderbook.Exchanges[orderbook.Indodax].Books[orderbook.ETH_IDR]
+	idx_usdt_idr = orderbook.Exchanges[orderbook.Indodax].Books[orderbook.USDT_IDR]
 	s.T().Log(start)
 	s.T().Log(end)
 	s.T().Log(qty)
+	s.T().Log(err)
 	assert.False(err != nil)
 	assert.Equal(1021.5982950714285, qty)
 	assert.Equal(((((start/150)*(1-0.001))-0.01)*2283000*(1-0.003)/14700*(1-0.003))-5, end)
+
+	receivedETH = start / 150
+	if best1.Qty-receivedETH == 0 {
+		assert.NotEqual(best1.Price, binance_eth_usdt.LowPriceSellSide().Price)
+	} else {
+		assert.Equal(best1.Qty-receivedETH, binance_eth_usdt.LowPriceSellSide().Qty)
+	}
+	soldETH = receivedETH*(1-0.001) - 0.01
+	if best2.Qty-soldETH == 0 {
+		assert.NotEqual(best2.Price, idx_eth_idr.TopPriceBuySide().Price)
+	} else {
+		assert.Equal(best2.Qty-soldETH, idx_eth_idr.TopPriceBuySide().Qty)
+	}
+	receivedUSDT = soldETH * 2283000 * (1 - 0.003) / 14700 //before fees
+	if best3.Qty-receivedUSDT == 0 {
+		assert.NotEqual(best3.Price, idx_usdt_idr.LowPriceSellSide().Price)
+	} else {
+		assert.Equal(best3.Qty-receivedUSDT, idx_usdt_idr.LowPriceSellSide().Qty)
+	}
 }
 
-func CreateDummyExchangesProfitable() {
+func CreateDummyExchangesProfitable() (orderbook.Order, orderbook.Order, orderbook.Order) {
 	orderbook.InitExchanges()
 	binance_eth_usdt := orderbook.Exchanges[orderbook.Binance].Books[orderbook.ETH_USDT]
 	binance_eth_usdt.AddBuy(orderbook.Order{
@@ -92,16 +137,18 @@ func CreateDummyExchangesProfitable() {
 		Price: 151,
 		Qty:   3,
 	})
-	binance_eth_usdt.AddSell(orderbook.Order{
+	bestPrice1 := orderbook.Order{
 		Price: 150,
 		Qty:   3,
-	})
+	}
+	binance_eth_usdt.AddSell(bestPrice1)
 
 	idx_eth_idr := orderbook.Exchanges[orderbook.Indodax].Books[orderbook.ETH_IDR]
-	idx_eth_idr.AddBuy(orderbook.Order{
+	bestPrice2 := orderbook.Order{
 		Price: 2283000,
 		Qty:   3,
-	})
+	}
+	idx_eth_idr.AddBuy(bestPrice2)
 	idx_eth_idr.AddBuy(orderbook.Order{
 		Price: 2282000,
 		Qty:   1,
@@ -144,12 +191,15 @@ func CreateDummyExchangesProfitable() {
 		Price: 14710,
 		Qty:   250,
 	})
-	idx_usdt_idr.AddSell(orderbook.Order{
+	bestPrice3 := orderbook.Order{
 		Price: 14700,
 		Qty:   250,
-	})
+	}
+	idx_usdt_idr.AddSell(bestPrice3)
+
+	return bestPrice1, bestPrice2, bestPrice3
 }
-func CreateDummyExchangesNotProfitableQty() {
+func CreateDummyExchangesNotProfitableQty() (orderbook.Order, orderbook.Order, orderbook.Order) {
 	orderbook.InitExchanges()
 	binance_eth_usdt := orderbook.Exchanges[orderbook.Binance].Books[orderbook.ETH_USDT]
 	binance_eth_usdt.AddBuy(orderbook.Order{
@@ -172,16 +222,18 @@ func CreateDummyExchangesNotProfitableQty() {
 		Price: 151,
 		Qty:   1,
 	})
-	binance_eth_usdt.AddSell(orderbook.Order{
+	bestPrice1 := orderbook.Order{
 		Price: 150,
 		Qty:   1,
-	})
+	}
+	binance_eth_usdt.AddSell(bestPrice1)
 
 	idx_eth_idr := orderbook.Exchanges[orderbook.Indodax].Books[orderbook.ETH_IDR]
-	idx_eth_idr.AddBuy(orderbook.Order{
+	bestPrice2 := orderbook.Order{
 		Price: 2283000,
 		Qty:   3,
-	})
+	}
+	idx_eth_idr.AddBuy(bestPrice2)
 	idx_eth_idr.AddBuy(orderbook.Order{
 		Price: 2282000,
 		Qty:   1,
@@ -224,12 +276,14 @@ func CreateDummyExchangesNotProfitableQty() {
 		Price: 14710,
 		Qty:   145,
 	})
-	idx_usdt_idr.AddSell(orderbook.Order{
+	bestPrice3 := orderbook.Order{
 		Price: 14700,
 		Qty:   145,
-	})
+	}
+	idx_usdt_idr.AddSell(bestPrice3)
+	return bestPrice1, bestPrice2, bestPrice3
 }
-func CreateDummyExchangesNotProfitable() {
+func CreateDummyExchangesNotProfitable() (orderbook.Order, orderbook.Order, orderbook.Order) {
 	orderbook.InitExchanges()
 	binance_eth_usdt := orderbook.Exchanges[orderbook.Binance].Books[orderbook.ETH_USDT]
 	binance_eth_usdt.AddBuy(orderbook.Order{
@@ -252,16 +306,18 @@ func CreateDummyExchangesNotProfitable() {
 		Price: 155,
 		Qty:   1,
 	})
-	binance_eth_usdt.AddSell(orderbook.Order{
+	bestPrice1 := orderbook.Order{
 		Price: 154,
 		Qty:   1,
-	})
+	}
+	binance_eth_usdt.AddSell(bestPrice1)
 
 	idx_eth_idr := orderbook.Exchanges[orderbook.Indodax].Books[orderbook.ETH_IDR]
-	idx_eth_idr.AddBuy(orderbook.Order{
+	bestPrice2 := orderbook.Order{
 		Price: 2273000,
 		Qty:   3,
-	})
+	}
+	idx_eth_idr.AddBuy(bestPrice2)
 	idx_eth_idr.AddBuy(orderbook.Order{
 		Price: 2272000,
 		Qty:   1,
@@ -304,8 +360,11 @@ func CreateDummyExchangesNotProfitable() {
 		Price: 14710,
 		Qty:   145,
 	})
-	idx_usdt_idr.AddSell(orderbook.Order{
+	bestPrice3 := orderbook.Order{
 		Price: 14700,
 		Qty:   145,
-	})
+	}
+	idx_usdt_idr.AddSell(bestPrice3)
+
+	return bestPrice1, bestPrice2, bestPrice3
 }
